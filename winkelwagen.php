821d51835d8e05e3b3cf5bc2256239da9c3af724
JesseMolhoek <jmolhoek12@gmail.com>
2018-11-01 11:25:21 +0100

    2

--- winkelwagen.php
+++ winkelwagen.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <title>WWI WEBSHOP!</title>
 
-    <link rel="stylesheet" type ="text/css" href="style.css">
+    <link rel="stylesheet" type ="text/css" href="webdesign.css">
 </head>
+
+
 <body>
 
 <h1> welkom </h1>
 
 <p> op deze stylvolle site </p>
-<?php
 
-?>
 </body>
 </html>
 
