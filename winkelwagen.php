cf9a4a9914ca062d532cfe309af5329c41759742
JesseMolhoek <jmolhoek12@gmail.com>
2018-11-01 11:17:40 +0100

    1

--- winkelwagen.php
+++ winkelwagen.php
@@ -1,7 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>WWI WEBSHOP!</title>
+
+    <link rel="stylesheet" type ="text/css" href="style.css">
+</head>
+<body>
+
+<h1> welkom </h1>
+
+<p> op deze stylvolle site </p>
 <?php
-/**
- * Created by PhpStorm.
- * User: jmolh
- * Date: 1-11-2018
- * Time: 11:12
- */
+
+?>
+</body>
+</html>
+
